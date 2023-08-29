234617b3712a66dff4710d7472d0474c57408692
Santiago <elsantoposada@gmail.com>
2023-08-29 09:35:55 -0300

    First commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "mi primer tareaa",
+  },
+  {
+    id: 1,
+    title: "mi sec tarea",
+    description: "mi sec tareaa",
+  },
+  {
+    id: 2,
+    title: "mi terce tarea",
+    description: "mi terce tareaa",
+  },
+];
